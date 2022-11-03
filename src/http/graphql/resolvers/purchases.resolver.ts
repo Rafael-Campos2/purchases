547b3dd113411d4b52716bda8908434360a29561
Rafael Campos <rafaelsilvamz111@gmail.com>
2022-11-03 08:55:20 -0300

    feat(purchases): add create purchase resolver

--- src/http/graphql/resolvers/purchases.resolver.ts
+++ src/http/graphql/resolvers/purchases.resolver.ts
@@ -1,8 +1,18 @@
 import { UseGuards } from '@nestjs/common';
-import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
+import { CustomersService } from 'src/services/customers.service';
 import { ProductsService } from 'src/services/products.service';
 import { PurchasesService } from 'src/services/purchases.service';
+import { CreatePurchaseInput } from '../inputs/create-purchase-input';
 import { Product } from '../models/product';
 import { Purchase } from '../models/purchases';
 
@@ -11,6 +21,7 @@ export class PurchasesResolver {
   constructor(
     private purchasesService: PurchasesService,
     private productsService: ProductsService,
+    private customersService: CustomersService,
   ) {}
 
   @Query(() => [Purchase])
@@ -25,5 +36,27 @@ export class PurchasesResolver {
     purchase: Purchase,
   ) {
     return this.productsService.findProductById(purchase.productId);
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Mutation(() => Purchase)
+  async createPurchase(
+    @Args('data') data: CreatePurchaseInput,
+    @CurrentUser() authUser: AuthUser,
+  ) {
+    let customer = await this.customersService.getCustomerByAuthUserId(
+      authUser.sub,
+    );
+
+    if (!customer) {
+      customer = await this.customersService.createCustomer({
+        authUserId: authUser.sub,
+      });
+    }
+
+    return this.purchasesService.createPurchase({
+      customerId: customer.id,
+      productId: data.productId,
+    });
   }
 }
